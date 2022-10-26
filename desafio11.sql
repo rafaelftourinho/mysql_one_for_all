c28c3719cfdc95051b450d9cd4163cce503878b9
rafaelftourinho <rafaelftourinho@gmail.com>
2022-10-26 18:58:24 -0300

    :sparkles: req 11

---
+++ desafio11.sql
@@ -0,0 +1,16 @@
+SELECT
+  m.musica AS nome_musica,
+  CASE
+    WHEN m.musica LIKE '%Bard%' THEN REPLACE(m.musica, 'Bard', 'QA')
+    WHEN m.musica LIKE '%Amar%' THEN REPLACE(m.musica, 'Amar', 'Code Review')
+    WHEN m.musica LIKE '%Pais%' THEN REPLACE(m.musica, 'Pais', 'Pull Requests')
+    WHEN m.musica LIKE '%SOUL%' THEN REPLACE(m.musica, 'SOUL', 'CODE')
+    WHEN m.musica LIKE '%SUPERSTAR%' THEN REPLACE(m.musica, 'SUPERSTAR', 'SUPERDEV')
+  END as novo_nome
+FROM SpotifyClone.musicas AS m
+WHERE m.musica LIKE '%Bard%'
+  OR m.musica LIKE '%Amar%'
+  OR m.musica LIKE '%Pais'
+  OR m.musica LIKE '%SOUL%'
+  OR m.musica LIKE '%SUPERSTAR%'
+ORDER BY m.musica DESC;
